b9154fef6c9c9f0879c214347578477f85b7e896
risonakamo <hmykanae@gmail.com>
2024-04-12 16:10:51 -0400

    testing floatui

--- web/components/item-row/item-row.tsx
+++ web/components/item-row/item-row.tsx
@@ -1,3 +1,6 @@
+import {useState} from "react";
+import {autoPlacement,useFloating,useHover,useInteractions} from "@floating-ui/react";
+
 import testbat from "assets/test-bat.webp";
 
 import "./item-row.less";
@@ -9,30 +12,49 @@ interface ItemRowProps
 
 export function ItemRow(props:ItemRowProps):JSX.Element
 {
-  return <div className="item-row">
-    <div className="img">
-      <div className="item-contain">
-        <img src={testbat}/>
-      </div>
-    </div>
-    <div className="detail">
-      <div className="main-details">
-        <h1>Ghost Hand</h1>
-        <div className="sub-details">
-          <div className="detail-item">
-            <p className="first">Builds</p>
-            <p>10</p>
-          </div>
-          <div className="detail-item">
-            <p className="first">Likes</p>
-            <p>10</p>
-          </div>
+  const [isOpen,setIsOpen]=useState(false);
+  const {refs,floatingStyles,context}=useFloating({
+    open:isOpen,
+    onOpenChange:setIsOpen,
+    middleware:[autoPlacement({
+      allowedPlacements:["top","bottom"]
+    })]
+  });
+  const hover=useHover(context);
+  const {getReferenceProps,getFloatingProps}=useInteractions([hover]);
+
+  return <>
+    <div className="item-row">
+      <div className="img">
+        <div className="item-contain" ref={refs.setReference} {...getReferenceProps}>
+          <img src={testbat}/>
         </div>
       </div>
-      <div className="sort-stat">
-        <h2>Builds</h2>
-        <p>40</p>
+      <div className="detail">
+        <div className="main-details">
+          <h1>Ghost Hand</h1>
+          <div className="sub-details">
+            <div className="detail-item">
+              <p className="first">Builds</p>
+              <p>10</p>
+            </div>
+            <div className="detail-item">
+              <p className="first">Likes</p>
+              <p>10</p>
+            </div>
+          </div>
+        </div>
+        <div className="sort-stat">
+          <h2>Builds</h2>
+          <p>40</p>
+        </div>
       </div>
     </div>
-  </div>;
+
+    {isOpen && (
+      <div className="floating" ref={refs.setFloating} style={floatingStyles} {...getFloatingProps}>
+        huh
+      </div>
+    )}
+  </>;
 }
