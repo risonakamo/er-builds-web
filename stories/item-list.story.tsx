38b4208069f17563844309c455c3ed2459fe0829
risonakamo <hmykanae@gmail.com>
2024-04-13 13:10:57 -0400

    2nd example item list story

--- stories/item-list.story.tsx
+++ stories/item-list.story.tsx
@@ -1,7 +1,7 @@
 import type {Meta,StoryObj} from "@storybook/react";
 
 import {ItemList} from "components/item-list/item-list";
-import {itemStatsExample1} from "lib/sample-data";
+import {itemStatsExample1, itemStatsExample2} from "lib/sample-data";
 
 type Story=StoryObj<typeof ItemList>;
 
@@ -16,4 +16,10 @@ export default meta;
 
 export const normal:Story={
 
+};
+
+export const list2:Story={
+  args:{
+    itemStats:itemStatsExample2
+  }
 };
