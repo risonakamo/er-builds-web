9f001b1eab30f7a0d6a596a7765777eaf5a1eb04
risonakamo <hmykanae@gmail.com>
2024-04-21 20:00:22 -0400

    dropdowns functionality

--- web/components/build-selector/build-selector.tsx
+++ web/components/build-selector/build-selector.tsx
@@ -1,4 +1,4 @@
-import {useMemo} from "react";
+import {useMemo, useState} from "react";
 import _ from "lodash";
 
 import {Dropdown1, DropdownItem} from "components/dropdown1/dropdown1";
@@ -15,29 +15,82 @@ interface BuildSelectorProps
 /** build selector header for builds page */
 export function BuildSelector(props:BuildSelectorProps):JSX.Element
 {
+  // --- states
+  const [selectedCharacter,setSelectedCharacter]=useState<string|null>(null);
+  const [selectedWeapon,setSelectedWeapon]=useState<string|null>(null);
+
+
+
+
+  // --- derived states
   // convert datafiles to select options
-  const characterOptions=useMemo(():DropdownItem[]=>{
-    return _.map(props.datafiles,(datafile:ErDataFileDescriptor):DropdownItem=>{
+  const characterOptions:DropdownItem[]=useMemo(()=>{
+    return _(props.datafiles)
+    .map((datafile:ErDataFileDescriptor):DropdownItem=>{
       return {
         value:datafile.character,
         img:resolveCharacterImg(datafile.character),
-      }
-    });
+      };
+    })
+    .uniq()
+    .value();
   },[props.datafiles]);
 
+  // available weapon options, only if a character is selected
+  const weaponOptions:DropdownItem[]=useMemo(()=>{
+    if (!selectedCharacter)
+    {
+      return [];
+    }
+
+    return _(props.datafiles)
+    .filter((datafile:ErDataFileDescriptor):boolean=>{
+      return datafile.character==selectedCharacter;
+    })
+    .map((datafile:ErDataFileDescriptor):DropdownItem=>{
+      return {
+        value:datafile.weapon,
+      };
+    })
+    .uniq()
+    .value();
+  },[selectedCharacter,props.datafiles]);
+
+
+
+  // --- handlers
+  /** selected character with character select dropdown. set the selected character and clear
+   *  the weapon selection. */
+  function h_selectedCharacter(newCharacter:string):void
+  {
+    setSelectedCharacter(newCharacter);
+    setSelectedWeapon(null);
+  }
+
+  /** selected new weapon with weapon select dropdown. set the new weapon */
+  function h_selectedWeapon(newWeapon:string):void
+  {
+    setSelectedWeapon(newWeapon);
+  }
+
+
+
+  // --- render
   return <div className="build-selector">
     <div className="contain">
       <div className="select-row">
         <span className="row-title">Character</span>
         <span>
-          <Dropdown1 options={characterOptions} currentSelection={null} placeholder="Character"/>
+          <Dropdown1 options={characterOptions} currentSelection={selectedCharacter}
+            placeholder="Character" onSelectionChange={h_selectedCharacter}/>
         </span>
       </div>
 
       <div className="select-row">
         <span className="row-title">Weapon</span>
         <span>
-          <Dropdown1 options={[]} currentSelection={null} placeholder="Weapon"/>
+          <Dropdown1 options={weaponOptions} currentSelection={selectedWeapon} placeholder="Weapon"
+            onSelectionChange={h_selectedWeapon}/>
         </span>
       </div>
     </div>
